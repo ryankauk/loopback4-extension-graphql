c70af6bb828eb700d421ff19fbb50094c2c36c83
Ryan Kauk <rek.kauk@gmail.com>
2018-12-03 16:54:00 -0500

    clean up unused variables

--- examples/prisma-test/src/application.ts
+++ examples/prisma-test/src/application.ts
@@ -1,23 +1,10 @@
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig, Application} from '@loopback/core';
-import {
-  AuthenticationComponent,
-  AuthenticationBindings,
-} from '@loopback/authentication';
-import {
-  RestExplorerBindings,
-  RestExplorerComponent,
-} from '@loopback/rest-explorer';
-import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
+
 import {ServiceMixin} from '@loopback/service-proxy';
-import * as path from 'path';
-import {MySequence, MyGraphqlAdapter} from './sequence';
-import {
-  GraphQlComponent,
-  GraphQLBindings,
-  GraphQlServer,
-} from '@ryankauk/graphql';
+
+import {MyGraphqlAdapter} from './sequence';
+import {GraphQlComponent, GraphQLBindings} from '@ryankauk/graphql';
 
 export class PrismaTestApplication extends BootMixin(
   ServiceMixin(Application),
--- examples/prisma-test/src/sequence.ts
+++ examples/prisma-test/src/sequence.ts
@@ -1,46 +1,7 @@
-import {inject} from '@loopback/context';
 import {Server as HttpServer} from 'http';
 import {Server as HttpsServer} from 'https';
-import {
-  FindRoute,
-  InvokeMethod,
-  ParseParams,
-  Reject,
-  RequestContext,
-  RestBindings,
-  Send,
-  SequenceHandler,
-} from '@loopback/rest';
-import {
-  MiddlewareHander,
-  IMiddlewareResolver,
-  GraphqlAdapter,
-  GraphQLBindings,
-} from '@ryankauk/graphql';
-const SequenceActions = RestBindings.SequenceActions;
-import {AuthenticationBindings, AuthenticateFn} from '@loopback/authentication';
+import {GraphqlAdapter} from '@ryankauk/graphql';
 import {GraphQLServer} from 'graphql-yoga';
-export class MySequence implements SequenceHandler {
-  constructor(
-    @inject(SequenceActions.FIND_ROUTE) protected findRoute: FindRoute,
-    @inject(SequenceActions.PARSE_PARAMS) protected parseParams: ParseParams,
-    @inject(SequenceActions.INVOKE_METHOD) protected invoke: InvokeMethod,
-    @inject(SequenceActions.SEND) public send: Send,
-    @inject(SequenceActions.REJECT) public reject: Reject,
-  ) {}
-
-  async handle(context: RequestContext) {
-    try {
-      const {request, response} = context;
-      const route = this.findRoute(request);
-      const args = await this.parseParams(request, route);
-      const result = await this.invoke(route, args);
-      this.send(response, result);
-    } catch (err) {
-      this.reject(context, err);
-    }
-  }
-}
 
 export class MyGraphqlAdapter implements GraphqlAdapter<GraphQLServer> {
   defaultServer: GraphQLServer;
